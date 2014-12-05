f4160bbcf1266d9eee22ce93529c6fd2ee315422
iodak <o93.ivan@gmail.com>
2014-12-05 12:51:47 +0100

    runnable_hotplug: Add min, max cpus online sysfs

--- arch/arm/mach-msm/runnable_hotplug.c
+++ arch/arm/mach-msm/runnable_hotplug.c
@@ -57,9 +57,9 @@ static unsigned int nr_run_hysteresis = 2;		/* 1 / 2 thread */
 module_param_named(nr_run_hysteresis, nr_run_hysteresis, uint, 0644);
 
 static unsigned int default_threshold_level = 4;	/* 1 / 4 thread */
-module_param_named(default_threshold_level, default_threshold_level, uint, 0644);
-
 static unsigned int nr_run_thresholds[NR_CPUS];
+static unsigned int max_cpus = DEFAULT_MAX_CPUS;
+static unsigned int min_cpus = DEFAULT_MIN_CPUS;
 
 DEFINE_MUTEX(runnables_lock);
 
@@ -128,13 +128,14 @@ static unsigned int get_avg_nr_runnables(void)
 static int get_action(unsigned int nr_run)
 {
 	unsigned int nr_cpus = num_online_cpus();
-	int max_cpus = DEFAULT_MAX_CPUS;
-	int min_cpus = DEFAULT_MIN_CPUS;
+
+	if (nr_run < min_cpus || nr_run > max_cpus)
+		return 0;
 
 	if ((nr_cpus > max_cpus || nr_run < nr_cpus) && nr_cpus >= min_cpus)
 		return -1;
 
-	if (nr_cpus < min_cpus || nr_run > nr_cpus)
+	if ((nr_cpus < min_cpus || nr_run > nr_cpus) && nr_cpus <= max_cpus)
 		return 1;
 
 	return 0;
@@ -206,6 +207,7 @@ static void __ref runnables_work_func(struct work_struct *work)
 	}
 }
 
+/*************************************sysfs*******************************************/
 static ssize_t runnables_on_show (struct kobject *kobj, struct kobj_attribute *attr, 
 					char *buf)
 {
@@ -236,14 +238,66 @@ static ssize_t runnables_on_store(struct kobject *kobj, struct kobj_attribute *a
 	return count;
 }
 
+static ssize_t min_cpus_show (struct kobject *kobj, struct kobj_attribute *attr, 
+					char *buf)
+{
+	return sprintf(buf, "%u\n", min_cpus);
+}
+
+static ssize_t min_cpus_store(struct kobject *kobj, struct kobj_attribute *attr, 
+					const char *buf, size_t count)
+{
+	int ret;
+	unsigned int val;
+	ret = sscanf(buf, "%u", &val);
+	if (ret != 1 || val < 1 || val > 4)
+		return -EINVAL;
+	
+	if (val > max_cpus)
+		val = max_cpus;
+
+	min_cpus = val;
+
+	return count;
+}
+
+static ssize_t max_cpus_show (struct kobject *kobj, struct kobj_attribute *attr, 
+					char *buf)
+{
+	return sprintf(buf, "%u\n", max_cpus);
+}
+
+static ssize_t max_cpus_store(struct kobject *kobj, struct kobj_attribute *attr, 
+					const char *buf, size_t count)
+{
+	int ret;
+	unsigned int val;
+	ret = sscanf(buf, "%u", &val);
+	if (ret != 1 || val < 1 || val > 4)
+		return -EINVAL;
+	
+	if (val < min_cpus)
+		val = min_cpus;
+
+	max_cpus = val;
+
+	return count;
+}
 
 static struct kobj_attribute runnables_on_attribute =
 	__ATTR(runnables_on, 0644, runnables_on_show, runnables_on_store);
+static struct kobj_attribute min_cpus_attribute =
+	__ATTR(min_cpus, 0644, min_cpus_show, min_cpus_store);
+static struct kobj_attribute max_cpus_attribute =
+	__ATTR(max_cpus, 0644, max_cpus_show, max_cpus_store);
 
 static struct attribute *attrs[] = {
 	&runnables_on_attribute.attr,
+	&min_cpus_attribute.attr,
+	&max_cpus_attribute.attr,
 	NULL,
 };
+/*************************************sysfs end****************************************/
 
 static struct attribute_group attr_group = {
 	.attrs = attrs,
